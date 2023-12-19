2c0027428e5c4370f5218cbef2e2334aa3e1dc8f
Mika <1215stps@gmail.com>
2023-12-19 17:59:56 +0800

    init commit

---
+++ src/extension.js
@@ -0,0 +1,14 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
